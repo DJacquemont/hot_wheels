5731c71239529cda8c4605aeabc072b16d6844b9
Mike Timmerman <timmermanmike@hotmail.com>
2022-12-07 22:57:45 +0100

    comment and clean-up

--- kalman.py
+++ kalman.py
@@ -2,52 +2,78 @@ import numpy as np
 import pygame as pg
 import vision
 
-def move(_x, _y, psi, w, h, obj, surface):
-    x = int(_x*1000-25)
-    y = int(h-_y*1000-25)
+
+def move(_x: float, _y: float, psi: float, w: int, h: int, obj, surface):
+    """ move function: draw image of Thymio at its current state estimate
+    Inputs:
+        _x - estimated x-position of Thymio [m]
+        _y - estimated y-position of Thymio [m]
+        psi - estimated orientation of Thymio [rad]
+        w - width of window given in surface [pixels]
+        h - height of window given in surface [pixels]
+        obj - pygame image object of Thymio
+        surface - pygame window object
+    """
+    # Convert x,y to pixels
+    x = int(_x * 1000 - 25) / 2
+    y = int(h - _y * 1000 - 25) / 2
+    # Draw Thymio image on surface
     thymio = pg.transform.rotate(obj, psi*180/np.pi)
     surface.blit(thymio, (x, y))
 
 
-def drawLine(_x, _y, w, h, surface):
+def drawLine(_x: list, _y: list, w: int, h: int, surface):
+    """ drawLine function: draw trailing line indicating previous estimated positions of Thymio
+    Inputs:
+        _x - estimated x-positions of Thymio [m]
+        _y - estimated y-positions of Thymio [m]
+        w - width of window given in surface [pixels]
+        h - height of window given in surface [pixels]
+        surface - pygame window object
+    """
     count = 1
-    while count < 1000 and count <= len(_x):
-        x = int(_x[len(_x)-count]*1000)
-        y = int(h - _y[len(_x)-count]*1000)
-        surface.fill((int(255-255/1000*count), 0, 0), ((x, y), (3, 3)))
+    n_prev = 1000
+    while count < n_prev and count <= len(_x):
+        # Convert x,y to pixels
+        x = int(_x[len(_x)-count] * 1000) / 2
+        y = int(h - _y[len(_x)-count] * 1000) / 2
+        # Draw dots with fading red coloring
+        surface.fill(( int(255*(1-count/n_prev)),0,0 ), ((x, y), (3, 3)))
         count += 1
 
 
+def path(optimal_path, w, h, surface):
+    """ path function: draws the optimal path in a window
+    Inputs:
+        optimal_path - coordinates of the optimal path nodes [m]
+        w - width of window given in surface [pixels]
+        h - height of window given in surface [pixels]
+        surface - pygame window object
+    """
+    for i in range(len(optimal_path)):
+        pg.draw.circle(surface, (0, 255, 0), (int(optimal_path[i][0] * 1000), int(h - optimal_path[i][1] * 1000)), 5)
+
+
+def init_state(vid):
+    fetched = False
+    i = 0
+    pose, orientation = (None, None)
+    while (not fetched) and (i < 100):
+        odoMet = vision.fetchOdoMeters(vid)
+        if type(odoMet) != bool:
+            pose = odoMet[0:2]
+            orientation = odoMet[2]
+            fetched = True
+        i += 1
+    return pose, orientation
+
+
 class Kalman:
-    def __init__(self, _acc_variance: float, _type, init_x=False, videoFeed=False) -> None:
+    def __init__(self, _acc_variance: float, _type, init_x: np.array) -> None:
         self.type = _type
 
         # mean of state GRV
-        if not init_x:
-            if videoFeed:
-                fetched = False
-                i = 0
-                while (not fetched) and (i < 1000):
-                    odoMet = vision.fetchOdoMeters(videoFeed)
-                    if type(odoMet) != bool:
-                        pos = odoMet[0:2]
-                        angle = odoMet[2]
-                        fetched = True
-                    i += 1
-                if _type == "pose":
-                    try:
-                        self._x = np.hstack((pos, np.array([0, 0])))
-                    except:
-                        print("Did not find position")
-                elif _type == "orientation":
-                    try:
-                        self._x = np.array([angle, 0])
-                    except:
-                        print("Did not find angle")
-            else:
-                print("Initial conditions or video feed needs to be specified.")
-        else:
-            self._x = np.array(init_x)
+        self._x = np.array(init_x)
 
         self._acc_variance = _acc_variance
 
@@ -75,13 +101,15 @@ class Kalman:
             A = None
             Q = None
 
-        new_x = A.dot(self._x)
-        new_P = A.dot(self._P).dot(A.T) + Q
+        try:
+            new_x = A.dot(self._x)
+            new_P = A.dot(self._P).dot(A.T) + Q
+            self._x = new_x
+            self._P = new_P
+        except:
+            print("Could not update state prediction")
 
-        self._x = new_x
-        self._P = new_P
-
-    def update(self, meas_value: np.array, meas_variance: np.array, C : np.array):
+    def update(self, meas_value: np.array, meas_variance: np.array, C : np.array) -> None:
         # y = z - Cx
         # S = C P Ct + R
         # K = P Ct S^-1
@@ -97,16 +125,22 @@ class Kalman:
         if self.type == "pose":
             Sinv = np.linalg.inv(S)
             K = self._P.dot(C.T).dot(Sinv)
-            new_P = self._P - self._P.dot(C.T).dot(Sinv).dot(C).dot(self._P)
+            P = self._P - self._P.dot(C.T).dot(Sinv).dot(C).dot(self._P)
         elif self.type == "orientation":
             Sinv = 1 / S
             K = self._P.dot(C.T) * Sinv
-            new_P = self._P - self._P.dot(C.T).dot(C)*self._P*Sinv
+            P = self._P - self._P.dot(C.T).dot(C)*self._P*Sinv
+        else:
+            K = None
+            P = None
 
-        new_x = self._x + K.dot(y)
+        try:
+            new_x = self._x + K.dot(y)
 
-        self._P = new_P
-        self._x = new_x
+            self._P = P
+            self._x = new_x
+        except:
+            print("Could not update state estimate")
 
     @property
     def cov(self) -> np.array:
@@ -116,28 +150,4 @@ class Kalman:
     def mean(self) -> np.array:
         return self._x
 
-    @property
-    def px(self) -> float:
-        return self._x[0]
-
-    @property
-    def py(self) -> float:
-        return self._x[1]
-
-    @property
-    def vx(self) -> float:
-        return self._x[3]
-
-    @property
-    def vy(self) -> float:
-        return self._x[4]
-
-
-# Initialize kalman filter
-meas_variance_pos = np.array([[0.001, 0],
-                              [0, 0.001]])
-meas_variance_vel = np.array([[0.1, 0],
-                              [0, 0.1]])
-meas_variance_att = np.array([0.001])
-meas_variance_omega = np.array([0.1])
 
